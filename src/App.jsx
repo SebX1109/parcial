2dbf46566094aefe4b7c62758e2ce9794e8f4f31
SebX1109 <rinconsebas547@gmail.com>
2023-11-17 15:37:48 -0500

    Parcialito

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+const App = () => {
+
+
+  return (
+
+
+
+    <div>
+
+      <div className="bg-[url('./Imagenes/Recurso1.png')] bg-contain  bg-gray-700 min-h-screen">
+        <header class="flex justify-content-center py-3" >
+          <div>
+            <div>
+
+              <header className="d-flex justify-content-center py-3">
+
+              </header>
+            </div>
+
+          </div>
+
+
+        </header>
+      </div>
+    </div>
+
+
+
+
+  )
+
+
+}
+export default App;
